bb4e7c6a0da2fb38c95306acdb8b6a927a5b8df1
Deep Bhattacharyya <bhattacharyya.deep@gmail.com>
2025-07-12 16:02:15 +1000

    nav

--- src/app/_components/navbar/navbar.tsx
+++ src/app/_components/navbar/navbar.tsx
@@ -1,22 +1,24 @@
 
 import { getAuthState, getPageBySlug } from "@/lib/api";
 import NavbarClient from "./navbarClient";
+import { get } from "http";
 
 // Server component: fetches data, passes to client component
 export default function Navbar() {
   const components = getPageBySlug("components.json");
   const anonymousProfileImage = components.content.anonymousProfileImage;
+
+  const navFile = getPageBySlug("menu.json");
+  const navigationItems = navFile.content.navigation.map((item: any) => ({
+    name: item.title,
+    href: item.link,
+  }));
   const authState = getAuthState();
   const navigation = authState.isLoggedIn ? [
     { name: "Dashboard", href: "#", current: true },
     { name: "Team", href: "#", current: false },
     { name: "Projects", href: "#", current: false },
     { name: "Calendar", href: "#", current: false },
-  ] : [
-    { name: "Home", href: "#", current: true },
-    { name: "About", href: "#", current: false },
-    { name: "Contact", href: "#", current: false },
-    { name: "Login", href: "#", current: false }
-  ];
+  ] : navigationItems;
   return <NavbarClient logo={components.content.logo} navigation={navigation} authState={authState} anonymousProfileImage={anonymousProfileImage} />;
 }
