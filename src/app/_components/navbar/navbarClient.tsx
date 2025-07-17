d53186e27602677cdd3ee7689740519529786e05
Deep Bhattacharyya <bhattacharyya.deep@gmail.com>
2025-07-17 12:34:45 +1000

    Update navbarClient.tsx

--- src/app/_components/navbar/navbarClient.tsx
+++ src/app/_components/navbar/navbarClient.tsx
@@ -94,7 +94,7 @@ export default function NavbarClient({ logo, navigation, anonymousProfileImage, 
                       (item.current
                         ? "bg-blue-300"
                         : "text-gray-800 hover:bg-blue-200") +
-                      " rounded-md px-3 py-2 text-sm font-medium"
+                      " rounded-md px-3 py-2 text-sm font-semibold"
                     }
                     aria-current={item.current ? "page" : undefined}
                   >
@@ -169,7 +169,7 @@ export default function NavbarClient({ logo, navigation, anonymousProfileImage, 
                   (item.current
                     ? "bg-blue-200 text-black"
                     : "text-gray-800 hover:bg-blue-200 hover:text-black") +
-                  " block rounded-md px-3 py-2 text-base font-medium"
+                  " block rounded-md px-3 py-2 text-base font-semibold"
                 }
                 aria-current={item.current ? "page" : undefined}
               >
