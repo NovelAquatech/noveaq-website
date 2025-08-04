c0e567c64318e8abf5c37104601b7c4578249115
Somdotta Sarkar <jhilick.s@gmail.com>
2025-08-04 13:32:04 +0200

    Added about us content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,32 @@
 import Navbar from "../_components/navbar/navbar";
-
-
 import FooterSection from "../_components/footer/footer";
 import PartnersCarousel from "../_components/partners/partner";
+import { AboutUs } from "@/types/about-us";
+import { getPageBySlug } from "@/lib/api";
 
 export default function LaboratoryEquipmentPage() {
-  
+  const about: AboutUs = getPageBySlug("about-us.json");
+
   return (
     <main className="min-h-screen relative overflow-hidden">
-      <Navbar currentHref="/laboratory-equipment" />
-About Us
+      <Navbar currentHref="/about" />
+      <h2 className="text-4xl md:text-5xl font-bold text-gray-800 mb-14 mt-10 text-center">
+        {about.title}
+      </h2>
+      {about.sections.map((section) => (
+        <div className="container mx-auto my-4 px-6 py-4 rounded-lg bg-blue-100 max-w-[90%] lg:max-w-[90%] ">
+          <h4 className="text-2xl md:text-2xl font-medium text-gray-800 mb-14 text-left">
+            {section.name}
+          </h4>
+          <p
+            className="mb-8 mt-4 text-gray-600 text-lg md:text-lg"
+            dangerouslySetInnerHTML={{ __html: section.description ?? "" }}
+          ></p>
+        </div>
+      ))}
+
       <PartnersCarousel />
       <FooterSection />
     </main>
   );
 }
-
