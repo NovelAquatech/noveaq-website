f6e93e202da59837431258555313c32dd0533367
Ranjeet Singh <ranjeet@modelia.com>
2025-09-21 13:21:21 +0530

    Add meeting component

---
+++ src/app/_components/MeetingDialog.tsx
@@ -0,0 +1,32 @@
+'use client';
+import React from 'react';
+
+interface MeetingPopupProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+export default function MeetingPopup({ open, onClose }: MeetingPopupProps) {
+  if (!open) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+      <div className="relative bg-white rounded-lg shadow-xl w-11/12 max-w-5xl h-[85vh]">
+        {/* Close button */}
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
+        >
+          ✕
+        </button>
+
+        {/* Iframe container */}
+        <iframe
+          src="http://outlook.office.com/book/Test@novelaquatechgmail.onmicrosoft.com/?ismsaljsauthenabled"
+          className="w-full h-full rounded-b-lg"
+          scrolling="yes"
+        />
+      </div>
+    </div>
+  );
+}
